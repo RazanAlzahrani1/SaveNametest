0f7ae345ec3bf4631c96d159d252ef76c35c1263
Razan Alzahrani <1razanalzahrani1@gmail.com>
2024-04-22 22:25:03 +0300

    Initial Commit

---
+++ SaveName/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SaveName
+//
+//  Created by RazanAlzahrani on 13/10/1445 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SaveName/SaveNameApp.swift
@@ -0,0 +1,17 @@
+//
+//  SaveNameApp.swift
+//  SaveName
+//
+//  Created by RazanAlzahrani on 13/10/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct SaveNameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
